0d66c490b0b6bf755b86b8e66497087f8a87c3e4
VaibhavMatere <59862355+vaibhav18matere@users.noreply.github.com>
2021-11-22 20:48:41 +0530

    components added

---
+++ note-keeper/src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import React, { useState } from "react";
+import "./App";
+import Form from "./Components/Form";
+import TodoList from "./Components/TodoList";
+
+
+function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
+  return (
+    <div className="App">
+      <header>
+        <h1>React - To Do App </h1>
+        <Form
+          inputText={inputText}
+          todos={todos}
+          setTodos={setTodos}
+          setInputText={setInputText}
+        />
+        <TodoList todos={todos} />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ note-keeper/src/Components/TodoList.js
@@ -0,0 +1,17 @@
+import React from "react";
+import Todo from "./Todo";
+
+const TodoList = ({ todos }) => {
+    console.log(todos);
+    return (
+        <div className="todo-container">
+            <ul className="todo-list">
+                {todos.map((todo) => (
+                    <Todo key={todo.id} text={todo.text} id={todo.id} />
+                ))}
+            </ul>
+        </div>
+    );
+};
+
+export default TodoList;
